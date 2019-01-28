103051aa04d5a38071656f2a6e01d1cfb60db838
Ryan Temple <ryantemple@ryans-macbook-pro.local>
2019-01-27 22:23:51 -0500

    Initial Commit

---
+++ Foggy GlassesTests/Foggy_GlassesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Foggy_GlassesTests.swift
+//  Foggy GlassesTests
+//
+//  Created by Ryan Temple on 1/27/19.
+//  Copyright © 2019 Foggy Glasses. All rights reserved.
+//
+
+import XCTest
+@testable import Foggy_Glasses
+
+class Foggy_GlassesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
